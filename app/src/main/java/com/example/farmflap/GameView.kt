35401512e2c66482093397a788975960130979f8
BrianMorga <bmorga@ggc.edu>
2023-12-08 21:21:06 -0500

    added background, chicken, and worked on some of the logic

--- app/src/main/java/com/example/farmflap/GameView.kt
+++ app/src/main/java/com/example/farmflap/GameView.kt
@@ -1,4 +1,133 @@
 package com.example.farmflap
 
-class GameView {
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.util.DisplayMetrics
+import android.view.MotionEvent
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.WindowManager
+
+
+class GameView (context: Context, attrs: AttributeSet) : SurfaceView(context, attrs), Runnable {
+
+    private var ourHolder: SurfaceHolder = holder
+    private var canvas: Canvas = Canvas()
+    private var paint: Paint = Paint()
+
+    private var chicken: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.chicken)
+    private var background: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.background6)
+    private var topWM: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.windmill2)
+    private var bottomWM: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.windmill)
+
+    private var isPlaying: Boolean = false
+    private var chickenX: Float = 75f
+    private var chickenY: Float = 0f
+    private var wMillX: Float = 0f
+    private var wMillY: Float = 0f
+
+    private var velocity: Float = 0f
+    private var gravity: Float = 2f
+
+
+
+    init {
+        wMillX = width.toFloat()
+        wMillY = (Math.random() * (height - bottomWM.height)).toFloat()
+        isPlaying = true
+        chicken = Bitmap.createScaledBitmap(chicken, 50, 50, false)
+        start()
+        background = BitmapFactory.decodeResource(resources, R.drawable.background6)
+        val displayMetrics = DisplayMetrics()
+        (context.getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay.getMetrics(
+            displayMetrics
+        )
+        val screenWidth = displayMetrics.widthPixels
+        val screenHeight = displayMetrics.heightPixels
+
+        // Resize the background to match the screen size
+        background = Bitmap.createScaledBitmap(background, screenWidth, screenHeight, false)
+    }
+
+
+
+
+    override fun run() {
+        while (isPlaying) {
+            update()
+            draw()
+            control()
+        }
+    }
+
+    private fun start() {
+        Thread(this).start()
+    }
+
+    fun update() {
+        chickenY += velocity
+        velocity += gravity
+
+        if (chickenY > height - chicken.height || chickenY < 0) {
+            isPlaying = false
+        }
+
+        wMillX -= 5f
+
+        if (chickenX + chicken.width > wMillX && chickenX < wMillX + topWM.width && chickenY + chicken.height > wMillY + topWM.height) {
+            isPlaying = false
+        }
+
+        if (wMillX < -topWM.width) {
+            wMillX = width.toFloat()
+            wMillY = (Math.random() * (height - bottomWM.height)).toFloat()
+        }
+    }
+
+    private fun draw() {
+        if (ourHolder.surface.isValid) {
+            canvas = ourHolder.lockCanvas()
+            canvas.drawBitmap(background, 0f, 0f, paint)
+            canvas.drawBitmap(chicken, chickenX, chickenY, paint)
+            canvas.drawBitmap(topWM, wMillX, wMillY, paint)
+            canvas.drawBitmap(bottomWM, wMillX, wMillY + topWM.height + 200, paint)
+            ourHolder.unlockCanvasAndPost(canvas)
+        }
+    }
+
+    private fun control() {
+        try {
+            Thread.sleep(17)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun pause() {
+        isPlaying = false
+        try {
+           val thread = Thread.currentThread()
+            thread.join()
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+    fun resumeGame() {
+        isPlaying = true
+        start()
+    }
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                velocity = -30f
+            }
+        }
+        return true
+    }
+
+
 }
--- app/src/main/java/com/example/farmflap/MainActivity.kt
+++ app/src/main/java/com/example/farmflap/MainActivity.kt
@@ -1,87 +1,36 @@
 package com.example.farmflap
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.farmflap.ui.theme.FarmFlapTheme
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var gameView: GameView
+    private lateinit var startButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FarmFlapTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ){
-                    FarmFlapApp()
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        gameView = findViewById(R.id.gameView)
+        startButton = findViewById(R.id.startButton)
+
+        startButton.setOnClickListener {
+            gameView.resumeGame()
+            startButton.isEnabled = false
+            startButton.visibility = Button.INVISIBLE
         }
     }
-}
 
-@Composable
-fun MainScreen(onPlayClick: () -> Unit) {
-    //main screen ui
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        //play button
+    override fun onPause() {
+        super.onPause()
+        gameView.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        gameView.resumeGame()
     }
 }
-
-@Composable
-fun GameScreen(onPauseClick: () -> Unit, onRestartClick: () -> Unit) {
-    //game screen ui
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        //pause button
-        //restart button
-    }
-}
-
-@Composable
-fun FarmFlapApp(){
-    var isGameScreen by remember { mutableStateOf(false) }
-
-    if(isGameScreen){
-        GameScreen(
-            onPauseClick = { isGameScreen = false },
-            onRestartClick = { /*TODO*/ }
-        )
-}else {
-        MainScreen(
-            onPlayClick = { isGameScreen = true })
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainScreenPreview() {
-    MainScreen(onPlayClick = {})
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GameScreenPreview(){
-    GameScreen(onPauseClick = {}, onRestartClick = {})
-}
-
-
-
